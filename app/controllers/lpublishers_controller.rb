4d5bbf884896189c7c6b1579db42b2a1a68170cc
Pincelli ugo <pincelli.ugo@libero.it>
2019-03-07 17:25:47 +0100

    secondo commit di recupero

--- app/controllers/lpublishers_controller.rb
+++ app/controllers/lpublishers_controller.rb
@@ -1,2 +1,15 @@
 class LpublishersController < ApplicationController
+	def index()
+		#@lpub2 = Lpublisher.find(2)
+		#@lpub50 = Lpublisher.find(50)
+		#---@lpubs = Lpublisher.primi_n_record(1,4)
+		@lpubs = Lpublisher.ssqq(5)
+	end
+
+private
+
+    def lpublisher_params
+      params.require(:lpublisher).permit(:ID_EDITORE, :Nome)
+    end 
+
 end
--- app/models/lpublisher.rb
+++ app/models/lpublisher.rb
@@ -1,2 +1,8 @@
 class Lpublisher < ApplicationRecord
+	#scope ​:primi_n_record​, ->( minimo, massimo) { where( ​'id GT 1 AND id LT 4' ) }
+	#scope :primi_n_record, ->(minimo, massimo) { where('id > 1 and id < 4') }
+	#scope :primi_n_record, ->(minimo, massimo) { where('id > ? and id < ?'), minimo, massimo }
+	#scope :primi_n_record, -> {}
+	scope :ssqq, ->(limite) {where("id > 1").limit(limite)}
+
 end
